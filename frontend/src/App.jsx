e71c1014a7ee1bd43b2222f06a9dc6d2b285e862
priscila134 <151745062+priscila134@users.noreply.github.com>
2024-12-01 03:08:57 +0100

    schadepagina toevoegen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 // App.jsx
 import VoertuigenLijst from "./VoertuigenLijst";
+import SchadePagina from "./SchadePagina";
 
 const App = () => {
   return (
-    <div>
-      <h1>CarAndAll Voertuigen</h1>
-      
-    </div>
+      <div>
+          <h1>CarAndAll Voertuigen</h1>
+          <h2>Voertuigen</h2>
+          <VoertuigenLijst />
+          <SchadePagina /> 
+      </div>
   );
 };
 
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,35 @@
 
 const API_BASE_URL = "https://localhost:7040/api/Voertuigs";
+const SCHADE_API_URL = "https://localhost:7040/api/Schades";
 
 export const fetchFilteredVoertuigen = async (filters) => {
   const queryParams = new URLSearchParams(filters).toString();
   const response = await fetch(`${API_BASE_URL}/filter?${queryParams}`);
   if (!response.ok) {
     throw new Error("Failed to fetch voertuigen");
+  }
+  return response.json();
+};
+
+export const fetchSchades = async () => {
+  const response = await fetch(SCHADE_API_URL);
+  if (!response.ok) {
+    throw new Error("Fout bij het ophalen van schades");
+  }
+  const data = await response.json();
+  console.log("Schades opgehaald:", data);  
+  return data;
+};
+export const voegSchadeToe = async (schadeData) => {
+  const response = await fetch(SCHADE_API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(schadeData),
+  });
+  if (!response.ok) {
+    throw new Error("Fout bij het toevoegen van schade");
   }
   return response.json();
 };
